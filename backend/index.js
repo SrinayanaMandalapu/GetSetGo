2b12f640f85b4c1855c39f668b57315f6b7ae526
SrinayanaMandalapu <mandalapunayana@gmail.com>
2025-03-10 23:56:10 +0530

    Updated backend routes and smart packing list, removed unused utils file

--- backend/index.js
+++ backend/index.js
@@ -1,46 +1,50 @@
-// require('dotenv').config();
-// const express=require('express');
-// const app=express();
+require('dotenv').config();
+const express=require('express');
+const app=express();
 
-// const port=process.env.PORT;
-// const placesRouter=require("./routes/places.routes.js")
+const port=process.env.PORT;
+const placesRouter=require("./routes/places.routes.js")
 
-// app.use('/', placesRouter);
+app.use('/', placesRouter);
 
-// app.listen(port,()=>{
-//     console.log(`The app is running at ${port}`);
-// })
+app.listen(port,()=>{
+    console.log(`The app is running at ${port}`);
+})
 
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const dotenv = require('dotenv');
+//const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 // const tripRoutes = require('./routes/trips');
 // const authRoutes = require('./routes/auth'); // Import auth routes
 
-dotenv.config();
+const packingListRoutes = require("./routes/packingListRoutes");
+app.use("/api/packing-list", packingListRoutes);
 
-const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+// dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log('MongoDB connected');
-}).catch(err => {
-    console.log(err);
-});
+// const app = express();
+// const PORT = process.env.PORT || 5000;
 
-app.use('/api/trips', tripRoutes);
-app.use('/api/auth', authRoutes); // Use auth routes
+// app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Trip Itinerary Planner API');
-});
+// mongoose.connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// }).then(() => {
+//     console.log('MongoDB connected');
+// }).catch(err => {
+//     console.log(err);
+// });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// app.use('/api/trips', tripRoutes);
+// app.use('/api/auth', authRoutes); // Use auth routes
+
+// app.get('/', (req, res) => {
+//     res.send('Trip Itinerary Planner API');
+// });
+
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
